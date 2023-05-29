51845efa5ef09335f231d3d4495de2d81592eb21
galihfebrianto <galih.febrianto10@gmail.com>
2023-05-29 15:47:23 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+// const { addNoteHandler, getAllNotesHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: () => {},
+  },
+];
+
+module.exports = routes;
